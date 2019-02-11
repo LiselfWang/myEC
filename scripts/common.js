25e22c73089899411ba8bbc207491ca39fa32fde
Liself <1144203946@qq.com>
2019-02-11 12:53:41 +0800

    caculate price

--- scripts/common.js
+++ scripts/common.js
@@ -1,1 +1,31 @@
 window.apiBase = "http://www.siteo.com.cn:8888";
+
+
+function appendApiUrl(url){
+    if(url.indexOf('/') === 0){
+        return window.apiBase + url;
+    }else{
+        return window.apiBase + '/' + url;
+    }
+}
+
+function aapendApiUrlForArray(array, attrName){
+    for (var index = 0; index < array.length; index++) {
+        var element = array[index];
+        element[attrName] = appendApiUrl(element[attrName]);
+    }
+}
+
+function myAjax(url, settings){
+    if(settings) {
+        if(settings.headers){
+            settings.headers.token = "LISELF";
+        }else{
+            settings.headers = {
+                token: "LISELF"
+            };
+        }
+    }
+
+    $.ajax(appendApiUrl(url),settings);
+}
--- scripts/website/submitOrder.js
+++ scripts/website/submitOrder.js
@@ -2,17 +2,39 @@ $(function(){
     getProduct();
     
     function getProduct(){
-        $.ajax(window.apiBase + "/api/front/item", {
-            headers: {
-                token:"LISELF"
-            },
+        myAjax("/api/front/item", {
             dataType: "json",
             type:"get",
             success:function(data){
                 var templateData = $("#template").html();
+
+                aapendApiUrlForArray(data,'itemPic');
+
                 var realData = Mustache.render(templateData,{"productList":data});
                 $(".prodictSection").html(realData);
+
+                for(var i=0;i<data.length;i++){
+                    $(".check").eq(i).change(function(){
+                        sumUp();
+                    })
+                    $(".quantity").eq(i).change(function(){
+                        sumUp();
+                    })
+                }
+ 
+                function sumUp(){
+                    var money = 0;
+                    for(var i=0;i<data.length;i++){
+                        var number = parseInt($(".quantity").eq(i).val());
+                      if($(".check").eq(i).prop("checked")&&(number>=1)) {
+                        money += (data[i].itemPrice)*number;
+                        $("#money").html(money);
+                      }
+                    }
+                }
             }
         })
     }
+       
+       
 })
